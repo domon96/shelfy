f1ab13854c3c555f6ee51dac5e5b664f85e9036a
Maksymilian Truchan <maksymilian.truchan@motorolasolutions.com>
2022-11-19 17:56:36 +0100

    add repo

---
+++ src/main/java/com/shelfy/model/Item.java
@@ -0,0 +1,56 @@
+package com.shelfy.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.time.Instant;
+import java.util.Objects;
+
+@Entity
+public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @ManyToOne
+    @JoinColumn(name = "product_name")
+    private Product product;
+    private Instant expirationDate;
+    private String description;
+    private int count;
+
+    public int getId() {
+        return id;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public Instant getExpirationDate() {
+        return expirationDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Item item = (Item) obj;
+        return id == item.id && product == item.product && expirationDate == item.expirationDate && count == item.count && Objects.equals(description, item.getDescription());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, product, expirationDate, description, count);
+    }
+}
--- src/main/java/com/shelfy/repository/ItemRepository.java
+++ src/main/java/com/shelfy/repository/ItemRepository.java
@@ -1,9 +1,9 @@
 package com.shelfy.repository;
 
-import com.shelfy.model.Category;
+import com.shelfy.model.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CategoryRepository extends JpaRepository<Category, Integer> {
+public interface ItemRepository extends JpaRepository<Item, Integer> {
 }
