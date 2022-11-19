88bd152020084a8ffb52f0845bbe74b758ec617e
DOMINIK MAZUR <DOMINIK.MAZUR@motorolasolutions.com>
2022-11-19 17:39:19 +0100

    fix

--- src/main/java/com/shelfy/controller/CategoryController.java
+++ src/main/java/com/shelfy/controller/CategoryController.java
@@ -3,6 +3,7 @@ package com.shelfy.controller;
 import com.shelfy.model.Category;
 import com.shelfy.service.CategoryService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
