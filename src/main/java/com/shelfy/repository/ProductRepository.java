1560db7200a4506685329f714fdab3a706af4b20
Maksymilian Truchan <maksymilian.truchan@motorolasolutions.com>
2022-11-19 22:17:14 +0100

    ordered by exp date

--- src/main/java/com/shelfy/repository/ProductRepository.java
+++ src/main/java/com/shelfy/repository/ProductRepository.java
@@ -3,9 +3,11 @@ package com.shelfy.repository;
 import com.shelfy.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
     @Query("SELECT p FROM Product p WHERE p.category.id = ?1")
     List<Product> findByCategory(int categoryId);
