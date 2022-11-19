6e24576c81933ee1102e3bc73ef0a629ed84dc16
Maksymilian Truchan <maksymilian.truchan@motorolasolutions.com>
2022-11-19 23:30:05 +0100

    fix

--- src/main/java/com/shelfy/configuration/WebMvcConfiguration.java
+++ src/main/java/com/shelfy/configuration/WebMvcConfiguration.java
@@ -1,5 +1,6 @@
 package com.shelfy.configuration;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -10,7 +11,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 @EnableScheduling
-@EnableConfigurationProperties(WebMvcConfiguration.class)
+@ConfigurationProperties("web-mvc-config")
 public class WebMvcConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
