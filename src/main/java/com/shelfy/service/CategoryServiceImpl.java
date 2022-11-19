0f72d9f5ba3edf8d887d543347a33ba522aaafcc
Maksymilian Truchan <maksymilian.truchan@motorolasolutions.com>
2022-11-19 15:07:54 +0100

    add controllers

--- src/main/java/com/shelfy/service/CategoryServiceImpl.java
+++ src/main/java/com/shelfy/service/CategoryServiceImpl.java
@@ -1,9 +1,11 @@
 package com.shelfy.service;
 
 import com.shelfy.model.Category;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class CategoryServiceImpl implements CategoryService{
     @Override
     public List<Category> getCategories() {
--- src/main/java/com/shelfy/service/ProductServiceImpl.java
+++ src/main/java/com/shelfy/service/ProductServiceImpl.java
@@ -2,10 +2,12 @@ package com.shelfy.service;
 
 import com.shelfy.model.Category;
 import com.shelfy.model.Product;
+import org.springframework.stereotype.Service;
 
 import java.time.Instant;
 import java.util.List;
 
+@Service
 public class ProductServiceImpl implements ProductService{
     @Override
     public List<Product> getProductsByCategory(int id) {
