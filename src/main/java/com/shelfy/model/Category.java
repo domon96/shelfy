7d7530a872aaac7ade29cff2d3a3a6717f48ee05
DOMINIK MAZUR <DOMINIK.MAZUR@motorolasolutions.com>
2022-11-19 14:29:26 +0100

    added models

---
+++ src/main/java/com/shelfy/model/Category.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) Motorola Solutions, INC.
+ * All Rights Reserved.
+ */
+
+package com.shelfy.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return id == category.id && Objects.equals(name, category.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+}
