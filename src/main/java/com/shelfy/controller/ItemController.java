adc35821d3331692bb5783763a650e0a2bed7cbc
Maksymilian Truchan <maksymilian.truchan@motorolasolutions.com>
2022-11-19 23:00:02 +0100

    fix

--- src/main/java/com/shelfy/controller/ItemController.java
+++ src/main/java/com/shelfy/controller/ItemController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(name = "/api")
+@RequestMapping(path = "/api")
 public class ItemController {
     private final ItemService itemService;
 
--- src/main/java/com/shelfy/controller/ProductController.java
+++ src/main/java/com/shelfy/controller/ProductController.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping(name = "/api")
+@RequestMapping(path = "/api")
 public class ProductController {
     private final ProductService productService;
     private final CategoryService categoryService;
